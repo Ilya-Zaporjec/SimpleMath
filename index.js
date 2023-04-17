1d08624b3daefa00368c2cb8ccb027197df802f4
IlyaZaporojec <ilya.zaporojec@gmail.com>
2023-04-17 23:47:28 +0300

    more math

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let result;
+let totalCount;
 let rightAnswer = 0;
 let wrongAnswer = 0;
+let level = 0;
 
 function generateRandomNumbers() {
   let firstNumber = Math.floor(Math.random() * 100) + 1;
@@ -22,23 +24,38 @@ function generateRandomNumbers() {
 }
 
 generateRandomNumbers();
+
+if (localStorage.getItem("level")) {
+  level = parseInt(localStorage.getItem("level"));
+  document.getElementById("level").textContent = `Level: ${level}`;
+}
+
 document.getElementById("checkButton").addEventListener("click", function () {
   let userInput = document.getElementById("userAnswer").value;
 
   let message;
   if (result == userInput) {
-    message = "Результат вірний!Молодець";
+    message = "Результат вірний!Так тримати!";
     document.getElementById("result").innerHTML = message;
-    document.getElementById("result").style.color = "#4CAF50";
+    document.getElementById("result").style.color = "blue";
     rightAnswer += 1;
+    if (rightAnswer % 10 == 0) {
+      level += 1;
+      document.getElementById("level").textContent = `Level: ${level}`;
+    }
   } else {
     message = "Результат невірний!Будь уважніше.";
     document.getElementById("result").innerHTML = message;
     document.getElementById("result").style.color = "red";
     wrongAnswer += 1;
+    if (wrongAnswer % 20 == 0) {
+      level -= 1;
+      document.getElementById("level").textContent = `Level: ${level}`;
+    }
   }
 
   generateRandomNumbers();
+  generateSecondRandomNumbers();
 
   document.getElementById("userAnswer").value = "";
 
@@ -48,4 +65,58 @@ document.getElementById("checkButton").addEventListener("click", function () {
   document.getElementById(
     "wrongAnswer"
   ).textContent = `Неправильно: ${wrongAnswer}`;
+
+  // Inside the event listener function
+  localStorage.setItem("level", level.toString());
+});
+
+function generateSecondRandomNumbers() {
+  let firstNumber = Math.floor(Math.random() * 10) + 1;
+  let secondNumber = Math.floor(Math.random() * 10) + 1;
+
+  totalCount = firstNumber * secondNumber;
+
+  document.getElementById("firstCount").textContent = firstNumber;
+  document.getElementById("secondCount").textContent = secondNumber;
+}
+generateSecondRandomNumbers();
+
+document.getElementById("checkBtn").addEventListener("click", function () {
+  let userInput = document.getElementById("userResult").value;
+
+  let message;
+  if (totalCount == userInput) {
+    message = "Результат вірний!Так тримати!";
+    document.getElementById("result").innerHTML = message;
+    document.getElementById("result").style.color = "blue";
+    rightAnswer += 1;
+    if (rightAnswer % 10 == 0) {
+      level += 1;
+      document.getElementById("level").textContent = `Level: ${level}`;
+    }
+  } else {
+    message = "Результат невірний!Будь уважніше.";
+    document.getElementById("result").innerHTML = message;
+    document.getElementById("result").style.color = "red";
+    wrongAnswer += 1;
+    if (wrongAnswer % 20 == 0) {
+      level -= 1;
+      document.getElementById("level").textContent = `Level: ${level}`;
+    }
+  }
+
+  generateSecondRandomNumbers();
+  generateRandomNumbers();
+
+  document.getElementById("userResult").value = "";
+
+  document.getElementById(
+    "rightAnswer"
+  ).textContent = `Правильно: ${rightAnswer}`;
+  document.getElementById(
+    "wrongAnswer"
+  ).textContent = `Неправильно: ${wrongAnswer}`;
+
+  // Inside the event listener function
+  localStorage.setItem("level", level.toString());
 });
